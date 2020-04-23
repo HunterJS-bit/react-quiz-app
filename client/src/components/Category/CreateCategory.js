c4cdd326ded013dc288da49670397ff3de07c8fb
Marko <markomiljkovic2@gmail.com>
2020-04-23 20:51:51 +0200

    added when saving category

--- client/src/components/Category/CreateCategory.js
+++ client/src/components/Category/CreateCategory.js
@@ -6,12 +6,14 @@ import { axiosInstance } from "../../util/axios";
 const CreateCategory = () => {
 
   const [name, setName] = useState('');
+  const [slug, setSlug] = useState('');
 
 
   const submitForm = async (e) => {
       e.preventDefault();
       const category = {
         name,
+        slug
       };
       await axiosInstance.post('/category', category);
   };
@@ -19,9 +21,18 @@ const CreateCategory = () => {
   return (<div className="category-create">
     <form onSubmit={submitForm}>
       <p>Create category</p>
+      <p>
+        <label>
+          Category Url
+          <input type='text' value={slug} onChange={(e) => { setSlug(e.target.value)}} />
+        </label>
+      </p>
+      <p>
       <label>
+        Category Name
         <input type='text' value={name} onChange={(e) => { setName(e.target.value)}} />
       </label>
+      </p>
       <button type="submit">Create Category</button>
     </form>
   </div>);
--- server/src/interfaces/category.interface.ts
+++ server/src/interfaces/category.interface.ts
@@ -2,5 +2,6 @@ import { Document } from 'mongoose';
 
 export interface CategoryInterface extends Document {
   readonly name: string;
+  readonly slug: string;
   readonly parent: object;
 }
