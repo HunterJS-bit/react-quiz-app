001dba9ff9732a05aad622a74fa8af186583614e
Marko <markomiljkovic2@gmail.com>
2020-05-13 01:27:14 +0200

    added description field to category model

--- server/src/models/category.schema.ts
+++ server/src/models/category.schema.ts
@@ -2,5 +2,6 @@ import * as mongoose from 'mongoose';
 
 export const CategorySchema = new mongoose.Schema({
   name: { type:String, required:true, unique: true },
+  description: { type:String, unique: true },
   slug: { type:String, required:true, unique: true },
 });
