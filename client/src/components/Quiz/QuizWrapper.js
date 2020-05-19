ee2edbeb0c13977627a030d0847850fdcd11fa45
Marko <markomiljkovic2@gmail.com>
2020-05-20 00:39:14 +0200

    added progress bar

--- client/src/components/Quiz/QuizWrapper.js
+++ client/src/components/Quiz/QuizWrapper.js
@@ -5,24 +5,31 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 const QuizWrapper = (props) => {
 	const { questions } = props;
 	const [questionIndex, setIndex] = useState(0);
+	const [completed, setCompleted] = useState(1);
 	const question = questions[questionIndex];
 	const totalQuestions = questions.length;
-
+	const percentage = 100 / totalQuestions;
 
 	const changeQuestion = () => {
 		setIndex((prev) => {
 			return prev + 1;
 		});
+		setCompleted((prev) => {
+			return prev + percentage;
+		});
 	};
+
 
 
 	return (
 		<div className='quiz'>
-			{/* <h2>quiz wrapper </h2> */}
+			<LinearProgress variant="determinate" value={completed} />
 			<p>
 				{questionIndex + 1} / {totalQuestions}
 			</p>
-			<Quiz questions={question} changeQuestion={changeQuestion} currentIndex={questionIndex+1} totalQuestions={totalQuestions} />
+			<Quiz questions={question} 
+				  changeQuestion={changeQuestion} 
+				  currentIndex={questionIndex+1}  />
 		</div>
 	);
 };
