8051adc15f750c2e5552ceb662877e64d831005a
marko <markomiljkovic2@gmail.com>
2020-07-14 00:57:07 +0200

    fix issue with character in pdf report

--- server/src/quiz/utils/pdfBilder.ts
+++ server/src/quiz/utils/pdfBilder.ts
@@ -1,11 +1,13 @@
 import * as PDFDocument from 'pdfkit';
+let font = __dirname + '/../../assets/Roboto-Regular.ttf';
 
 const pdfBuffer = (data) => {
     return new Promise(resolve => {
         const doc = new PDFDocument({ margin: 50 });
-        // console.log('dataaaaa ', data);
+        doc.registerFont('Roboto', font);
         generateHeader(doc);
-        // doc.text('hello world', 100, 50)
+        generateHeading(doc, data.name);
+        generateMain(doc, data.questions);
         generateFooter(doc);
         doc.end()
     
@@ -25,7 +27,8 @@ const generatePDF = async (data) => {
 
 function generateFooter(doc) {
     doc.text('This is a footer', 20, doc.page.height - 50, {
-        lineBreak: false
+        lineBreak: false,
+        align: "center"
       });
 }
 
@@ -33,10 +36,42 @@ function generateHeader(doc) {
     doc
       .fillColor("#444444")
       .fontSize(20)
-      .text("ACME Inc.", 110, 57)
+      .text("Auto-testovi.com", 110, 57)
       .fontSize(10)
       .text("123 Main Street", 200, 65, { align: "right" })
       .text("New York, NY, 10025", 200, 80, { align: "right" })
       .moveDown();
 }
+
+function generateHeading(doc, title) {
+    doc
+    .fillColor("#444444")
+    .fontSize(24)
+    .text(`Auto testovi iz Testa broj 1`,  200, 80)
+    .moveDown();
+}
+
+function generateMain(doc, list) {
+    let i,
+    invoiceTableTop = 150;
+    list.map((e, i) => {
+        const position = invoiceTableTop + (i + 1) * 20;
+        generateTableRow(
+        doc,
+        position,
+        e.question,
+        );
+        doc.moveDown();
+       // doc.list(e.options,{numbered: true})
+    })
+}
+
+
+function generateTableRow(doc, y, c1, c2?, c3?, c4?, c5?) {
+    doc
+      .fontSize(10)
+      .font('Roboto')
+      .text('Kada policijsko vozilo sa prvenstvom prolaza daje istovremeno i svetlosni znak upozorenja (uzastopno ili naizmenično uključivanje dugih svetala), vozač vozila koje se kreće neposredno ispred policijskog vozila koje daje te znake mora:')
+      .moveDown()
+  }
 export default generatePDF;
